f2e12adb00e70690ee3ef4b67c72e13502d0f7fb
Thomas <tbre90@gmail.com>
2021-03-08 21:55:00 +0100

    Implement uniquifying of variable names.

--- src/uniquify.rs
+++ src/uniquify.rs
@@ -3,27 +3,31 @@ use crate::ast::{AstNode, Program};
 
 use std::collections::HashMap;
 
-fn uniquify_exp(env: &mut HashMap<String, String>, e: AstNode) -> AstNode {
+fn uniquify_exp(environments: &mut Vec<HashMap<String, String>>, e: AstNode) -> AstNode {
     match e {
         AstNode::Int(n) => AstNode::Int(n),
 
         AstNode::Var { name } => {
-            let new_name;
 
-            if env.contains_key(&name) {
-                new_name = env.get(&name).unwrap();
-            } else {
-                new_name = &name;
+            let mut new_name = name.clone();
+
+            let last = environments.len();
+
+            for i in (0..last).rev() {
+                if environments[i].contains_key(&name) {
+                    new_name = environments[i][&name].clone();
+                    break;
+                }
             }
 
             AstNode::Var {
-                name: new_name.to_string()
+                name: new_name
             }
         },
 
         AstNode::Prim { op, mut args } => {
             for i in 0..args.len() {
-                let new_arg_expr = uniquify_exp(env, args[i].clone());
+                let new_arg_expr = uniquify_exp(environments, args[i].clone());
                 args[i] = new_arg_expr;
             }
 
@@ -35,24 +39,20 @@ fn uniquify_exp(env: &mut HashMap<String, String>, e: AstNode) -> AstNode {
 
         AstNode::Let { var, value, in_exp, } => {
 
-            let mut number = 1;
+            environments.push(HashMap::new());
 
-            let mut new_name = var.clone() + "." + (&number.to_string());
+            let last = environments.len()-1;
+            let current_env = environments.get_mut(last).unwrap();
 
-            if env.contains_key(&var) {
-                while env.contains_key(&new_name) {
-                    number += 1;
-                    new_name = var.clone() + "." + (&number.to_string());
-                }
-            }
+            let new_name = var.clone() + "." + &(last+1).to_string();
 
-            //let unq_var = var.clone() + "." + (&number.to_string());
+            current_env.insert( var.clone(), new_name.clone() );
 
-            env.insert( var, new_name.clone() );
+            let unq_value = uniquify_exp(environments, *value);
 
-            let unq_value = uniquify_exp(env, *value);
+            let unq_body = uniquify_exp(environments, *in_exp);
 
-            let unq_body = uniquify_exp(env, *in_exp);
+            environments.pop();
 
             AstNode::Let {
                 var: new_name,
@@ -70,6 +70,6 @@ fn uniquify_exp(env: &mut HashMap<String, String>, e: AstNode) -> AstNode {
 pub fn uniquify_program(p: Program) -> Program {
     Program {
         info: p.info,
-        exp: uniquify_exp(&mut HashMap::new(), p.exp),
+        exp: uniquify_exp(&mut Vec::<HashMap<String, String>>::new(), p.exp),
     }
 }
